a810eea9f01fa2d3d0ff67debf767dd78ee3813e
Roderick Kemp <kemp954@icloud.com>
2025-07-18 21:25:10 -0400

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,9 +1,10 @@
-'use client'
+// lib/supabaseClient.ts
+'use client';
 
-import { createBrowserClient } from '@supabase/auth-helpers-nextjs'
-import { Database } from '@/types/supabase'
+import { createBrowserClient } from '@supabase/auth-helpers-nextjs';
+import { type Database } from '@/types/supabase'; // optional if you typed your DB
 
 export const supabase = createBrowserClient<Database>(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-)
+);
