f07f29f7467a4249c34bfcffceb5b1293276afbb
Roderick Kemp <you@agentgift.ai>
2025-08-18 20:04:02 -0700

    chore: backup before dialog refresh

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -1,5 +1,5 @@
-"use client";
-import { getBrowserClient } from "@/lib/supabase/clients";
+"use client"
+
 import * as React from "react"
 import * as DialogPrimitive from "@radix-ui/react-dialog"
 import { X } from "lucide-react"
@@ -21,7 +21,7 @@ const DialogOverlay = React.forwardRef<
   <DialogPrimitive.Overlay
     ref={ref}
     className={cn(
-      "fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+      "fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
       className
     )}
     {...props}
@@ -120,4 +120,3 @@ export {
   DialogTitle,
   DialogDescription,
 }
-
